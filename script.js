f7b5ac7b28576c9334f1e1e5266715f1773af243
AartiW-12 <aartiwhaval12@gmail.com>
2025-09-24 11:03:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 const chnageButton = document.getElementById("change_button");
-const resetButton = document.getElementById("Reset")
+const resetButton = document.getElementById("reset_button")
 const blockInput = document.getElementById("block_id");
 const colorInput = document.getElementById("colour_id");
 const gridItems = document.querySelectorAll(".grid-item")
@@ -10,18 +10,12 @@ function resetColors() {
 }
 
 chnageButton.addEventListener("click" , ()=> {
-	const blockID = blockInput.value;
+	const blockID = parseInt(blockInput.value , 10);
 	const color = colorInput.value;
 
 	if(blockID>=1 && blockID<=9 && color){
 		resetColors();
-		const target = document.getElementById(blockID);
-		if(target){
-			target.style.backgroundColor = color;
-		}
-	}
-	else{
-		console.log("Invalid Block Number")
+		gridItems[blockID - 1].style.backgroundColor = color
 	}
 });
 
